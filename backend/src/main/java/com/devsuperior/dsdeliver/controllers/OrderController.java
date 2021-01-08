453d8ea750a4f9faaa2f59186f6fdb0ea20b8c28
Saulo Alves <sauloealves.gmail.com>
2021-01-08 10:14:22 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.services.OrderService;
@@ -22,7 +26,19 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll(){
 		List<OrderDTO> list = service.findAll();
 		
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(list); // retorna o codigo 200 (OK)
 	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto); // retorna o codigo 201(inserido)
+		
+	}
+	
 	
 }
